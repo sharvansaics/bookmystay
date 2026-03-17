3d7529561e51826182cc23f00ca40d0b093b3c36
sharvansaics <sc1781@srmist.edu.in>
2026-03-17 09:15:45 +0530

    Initial commit for uc1

---
+++ week1and2/bookmystay/src/bookmystay.java
@@ -0,0 +1,16 @@
+// File: BookMyStay.java
+public class BookMyStay {
+
+    // Application entry point
+    public static void main(String[] args) {
+        // Print welcome message
+        System.out.println("===================================");
+        System.out.println(" Welcome to BookMyStay Application ");
+        System.out.println(" Version: 1.0.0 ");
+        System.out.println("===================================");
+        System.out.println("Thank you for choosing BookMyStay!");
+
+        // Terminate application
+        System.out.println("Application has terminated.");
+    }
+}
